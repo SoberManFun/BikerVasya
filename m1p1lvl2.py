9408caafad7d1046a4e3306507cf99ecbb2cd77b
SoberManFun <treg3359@gmail.com>
2023-06-13 23:27:27 +0300

    1 try

---
+++ m1p1lvl2.py
@@ -0,0 +1,17 @@
+# Модуль1.Часть1.Уровень2.начало
+mkad1 = 109
+speed1 = abs(int(input('Введите скорость Байкера Васи: ')))
+time1 = abs(int(input('Введите время Байкера Васи до остановки в часах: ')))
+s1 = speed1 * time1
+rcount1 = s1 // mkad1
+if rcount1 < 1:
+	s1 = s1 - (rcount1 * mkad1)
+#	s1 = (rcount1 - int(rcount1)) * mkad1
+#	s1 = round(s1)
+	print('Байкер Вася остановится на: ', s1, 'км')
+else:
+	s1 = s1 - (rcount1 * mkad1)
+#	s1 = (rcount1 - int(rcount1)) * mkad1
+#	s1 = round(s1)
+	print('Байкер Вася остановится на: ', s1, 'км')	
+# Модуль1.Часть1.Уровень2.конец
